6c28a5e9033b33fe8190b52483818eaf91ce1cbf
KSGarage <109337249+Kimsg130@users.noreply.github.com>
2022-11-16 04:22:18 +0900

    version-2.0.0
    2022-11-16 Updates
    주요 변경사항 : 디자인 수정, 애니메이션과 데이터 추가
    1. 로고 제작 및 페이지 디자인
    2. Tween.js이용해서 핀을 클릭하면 가까이 가겠끔 애니메이션 추가
    3. 데이터 추가

--- Project/src/JS/3D-space.js
+++ Project/src/JS/3D-space.js
@@ -10,9 +10,20 @@ var INTERSECTED = false;
 //TODO : 사건이 발생한 나라들을 저장하는 배열 추가
 //TODO : 한 연도에 못해도 6개는 만들기
 var year2022 = new Array("2022", "USA", "RUSSIA", "KOREA");
-var year2002 = new Array("2002","MEXICO", "KOREA");
+var year20CE = new Array("20CE","USA","ATLANTICOCEAN","SARAJEVO","KOREA","GERMAN","KOREAWAR","RUSSIA");
+var year16CE = new Array("16CE","IRELAND","GAYA","SPAIN","ENGLAND","JAPAN");
+var year1CE = new Array("1CE","SILA", "ROMA", "JERUSALEM", "GAYA", "POM");
+var yearBC1CE = new Array("BC1CE", "GOGURYU", "SILA", "JERUSALEM", "EGYPT");
+var yearBC3CE = new Array("BC3CE", "MACEDONIA", "TURKIYE","PERSIA", "BABYLON", "CHINA", "CHINA-HEHA", "EGYPT");
+
 //나라의 3차원 좌표를 저장하는 맵 -> key:[나라], value:[x, y, z]
-var countrys = {USA : [-20,70,68], RUSSIA : [-10,87,-47], KOREA : [-49,58,-63], MEXICO : [0,30,94]};
+var countrys = {USA : [-20,70,68], RUSSIA : [-10,87,-47], KOREA : [-49,58,-63], MEXICO : [0,30,94],
+                SILA : [-49.63,59.76,-61], ROMA : [72,67,-15], JERUSALEM : [67,54,-50], GAYA : [-49.99,55.75,-64], POM :[72,66,-18],
+                GOGURYU : [-47.95,62.15,-60.94], EGYPT :[79.68,41.94,-42.05], MACEDONIA : [68.40,66.74,-27.26], TURKIYE : [68.14, 62.77, -35.95],
+                PERSIA : [52.75,57,-62], BABYLON : [62.06,52.01,-57.62], CHINA : [-16.28,57.43,-79.45], "CHINA-HEHA" : [-25.77,56.09,-77.88],
+                ATLANTICOCEAN :[56.71,64.34,49], SARAJEVO:[64.92,67.95,-22.57],GERMAN:[61.07,76.48,-14.54],KOREAWAR:[-44.91,59.86,-61.16],
+                IRELAND:[58.48,79.47,7.35],SPAIN:[74.87,64.55,4.4],ENGLAND:[59.62,78.95,1.74],JAPAN:[-59.84,58.11,-53.22]
+};
 
 var slider = document.getElementById("slider-action");
 
@@ -34,7 +45,7 @@ function init(){
 
     //카메라 할당
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(-150, 150, -230); //최종 한국을 바라보는 좌표 (-150,150,-230)
+    camera.position.set(-120, 140, -150); //최종 한국을 바라보는 좌표 (-150,150,-230)
 
     //렌더러 할당
     renderer = new THREE.WebGLRenderer({
@@ -52,8 +63,10 @@ function init(){
     controls = new OrbitControls(camera, renderer.domElement);
     controls.enablePan = false;
     controls.enableDamping = true;
-    controls.dampingFactor = 0.04;
-    controls.rotateSpeed = 0.6;
+    controls.dampingFactor = 0.025;
+    controls.rotateSpeed = 0.4;
+    // controls.minDistance = 105;
+    controls.saveState();
     controls.update();
 
     //빛 할당
@@ -77,6 +90,7 @@ function init(){
 
     renderer.outputEncoding = THREE.sRGBEncoding;
     window.requestAnimationFrame(animate);
+    expressYear(2022);
 }
 
 
@@ -88,12 +102,13 @@ function animate() {
         resetMaterials();
     }
     renderer.render(scene, camera);
+    TWEEN.update();
     controls.update();
 }
 
 //3D오브젝트를 생성하는 메서드
 function makePoints(year){
-    const geometry = new THREE.SphereGeometry(4, 32, 16);
+    const geometry = new THREE.SphereGeometry(2.5, 32, 16);
     for(let i = 1; i<year.length; i++){
         let point = new THREE.Mesh(geometry);
         point.material = new THREE.MeshStandardMaterial({ 
@@ -107,7 +122,6 @@ function makePoints(year){
         point.position.set(coordinate[0],coordinate[1],coordinate[2]);
         point.userData.yc = year[0]+country;
         pins.add(point);
-        // scene.add(point);
     }
     scene.add(pins);
 }
@@ -120,12 +134,15 @@ function onMouseMove(event){
 
 //클릭이벤트가 발생했을 때, 레이와 교차한 객체를 판별하여 기능을 수행하는 메서드
 function onClick(event){
+    console.log(camera.position);
     raycaster.setFromCamera(mouse, camera);
     const pin = scene.getObjectByName("pins");
     const intersects = raycaster.intersectObjects(pin.children, false); //pins.children
     if(intersects.length >0){
+        focusInPin(intersects[0].object);
         expressContent(intersects[0].object.userData.yc);
         modalOn();
+        // console.log(camera.position);
     }
 }
 
@@ -164,21 +181,66 @@ function onWindowResize(){
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+//tween.js이용해서 눌렀을 때 핀 위치로 가는 애니메이션 구현 22-11-15
+function focusInPin(pin){
+    const coords = { x:camera.position.x, y:camera.position.y, z:camera.position.z};
+    new TWEEN.Tween(coords) 
+	    .to({x : pin.position.x*1.8, y : pin.position.y*1.8, z : pin.position.z*1.8}, 800) 
+        .easing(TWEEN.Easing.Circular.Out)
+	    .onUpdate(() => {
+            camera.position.set(coords['x'],coords['y'],coords['z']);
+	    })
+	    .start();
+}
+
 function yearChange(){
     let svalue = slider.value;
-    let year;
+    let year, yyyy;
     switch(svalue){
         case '5' :
             year = year2022;
+            yyyy = "2022";
             break;
         case '4' :
-            year = year2002;
+            year = year20CE;
+            yyyy = "20CE";
+            break;
+        case '3' :
+            year = year16CE;
+            yyyy = "16CE";
+            break;
+        case '2' :
+            year = year1CE;
+            yyyy = "1CE";
+            break;
+        case '1' :
+            year = yearBC1CE;
+            yyyy = "BC1CE";
+            break;
+        case '0' :
+            year = yearBC3CE;
+            yyyy = "BC3CE";
             break;
         default :
+            console.log("invaild value");
             year = year2022;
+            yyyy = "";
     }
     pins.clear();
+    expressYear(yyyy);
     makePoints(year);
+}
+
+function expressYear(yyyy){
+    const bigyear = document.querySelector('#bigyear');
+    const smallyear = document.querySelector('#smallyear');
+    bigyear.innerHTML = yyyy;
+    smallyear.innerHTML = yyyy;
+    smallyear.classList.add('animate__flipInX');
+    setTimeout(() => { bigyear.style.display = 'inline'; }, 300);
+    setTimeout(() => { bigyear.style.display = 'none'; }, 1100);
+    
+    setTimeout(() => { smallyear.classList.remove('animate__flipInX'); }, 2000);
 }
 
 //이벤트리스너
